5a8a9845540ca3373217993a16af065e6e9e331f
Rafael Ferreira <rafaelmfer@hotmail.com>
2024-10-13 22:18:39 -0700

    feat: create basic graphql server

---
+++ server/index.js
@@ -0,0 +1,30 @@
+const { ApolloServer, gql } = require("apollo-server-express");
+const express = require("express");
+
+const typeDefs = gql`
+    type Query {
+        hello: String
+    }
+`;
+
+const resolvers = {
+    Query: {
+        hello: () => "Hello World!",
+    },
+};
+
+async function startServer() {
+    const app = express();
+    const server = new ApolloServer({ typeDefs, resolvers });
+
+    await server.start();
+    server.applyMiddleware({ app });
+
+    app.listen({ port: 4000 }, () =>
+        console.log(
+            `🚀 Server ready at http://localhost:4000${server.graphqlPath}`
+        )
+    );
+}
+
+startServer();
